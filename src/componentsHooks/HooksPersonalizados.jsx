0ac75560f64581b9f1f747e4b7352bcbf52765cc
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:26:53 +0100

    Update HooksPersonalizados.jsx

--- src/componentsHooks/HooksPersonalizados.jsx
+++ src/componentsHooks/HooksPersonalizados.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {useFetch} from "./Hooks/useFetch.jsx";//importar el hook personalizado de la carpeta Hooks
+import {useFetch} from "./Hooks/useFetch.jsx";//Importar el hook personalizado de la carpeta Hooks
 
 export default function HooksPersonalizados(props){
     let url = "https://pokeapi.co/api/v2/pokemon/";
-    let {data, isPending, error} = useFetch(url);//sacando todas las cosas del hook
+    let {data, isPending, error} = useFetch(url);//Sacando todas las cosas del hook
     return(
         <>
             <h2>per</h2>
-            <p>{JSON.stringify(data.count)}</p>{/*lo que devuelva el hook ya esta listo para usar incluso si es asincrono*/}
+            <p>{JSON.stringify(data.count)}</p>{/*Lo que devuelva el hook ya esta listo para usar incluso si es asincrono*/}
         </>
     );
 }

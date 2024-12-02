e2030f16f911828396f3b83a42e44371d99e163f
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:37:41 +0100

    Update Eventos.jsx

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -6,11 +6,11 @@ export class EventosES6 extends Component {
         this.state = {
              contador: 0
         }
-        this.sumar = this.sumar.bind(this);//por cada funcion de evento
+        this.sumar = this.sumar.bind(this);//Por cada funcion de evento
         this.restar = this.restar.bind(this);
     }
     sumar(e){//e hace referencia al evento
-        this.setState({//cambiar el estado
+        this.setState({//Cambiar el estado
             contador: this.state.contador + 1
         });
     }
@@ -25,7 +25,7 @@ export class EventosES6 extends Component {
                 <h2>eventos</h2>
                 <p>{this.state.contador}</p>
                 <nav>
-                    <button onClick={this.sumar}>+</button>{/*para hacer los eventos se hace una funcion y se llama a la funcion asi, onClick es un evento pero funcionan todos los de html*/}
+                    <button onClick={this.sumar}>+</button>{/*Para hacer los eventos se hace una funcion y se llama a la funcion asi, onClick es un evento pero funcionan todos los de html*/}
                     <button onClick={this.restar}>-</button>
                 </nav>
             </div>
@@ -33,12 +33,12 @@ export class EventosES6 extends Component {
     }
 }
 
-//para ecmascript 7 hay otra manera, pero ambas funcionan
+//Para ecmascript 7 hay otra manera, pero ambas funcionan
 export class EventosES7 extends Component {
-    state = {//ahora se puede hacer el state talcual
+    state = {//Ahora se puede hacer el state talcual
          contador: 0
     }
-    sumar = (e) => {//ahora son arrow functions
+    sumar = (e) => {//Ahora son arrow functions
         this.setState({
             contador: this.state.contador + 1
         });
@@ -54,7 +54,7 @@ export class EventosES7 extends Component {
                 <h2>eventos nuevo</h2>
                 <p>{this.state.contador}</p>
                 <nav>
-                    <button onClick={this.sumar}>+</button>{/*para hacer los eventos se hace una funcion y se llama a la funcion asi, onClick es un evento pero funcionan todos los de html*/}
+                    <button onClick={this.sumar}>+</button>{/*Para hacer los eventos se hace una funcion y se llama a la funcion asi, onClick es un evento pero funcionan todos los de html*/}
                     <button onClick={this.restar}>-</button>
                 </nav>
             </div>
@@ -64,21 +64,21 @@ export class EventosES7 extends Component {
 
 
 function Boton(props){
-    return <button onClick={props.myOnClick}>boton comp</button>//poniendo el evento esta vez si con los props, esta function tambien se podria hacer con ()=>
+    return <button onClick={props.myOnClick}>boton comp</button>//Poniendo el evento esta vez si con los props, esta function tambien se podria hacer con ()=>
 }
 export class MasEventos extends Component{
     evento = (e, parametro) => {
-        console.log(e);//evento de react
-        console.log(e.nativeEvent);//evento de javascript normal
-        console.log(e.target);//etiqueta que genero el evento
+        console.log(e);//Evento de react
+        console.log(e.nativeEvent);//Evento de javascript normal
+        console.log(e.target);//Etiqueta que genero el evento
         console.log(parametro);
     }
     render(){
         return(
             <div>
                 <h2>mas eventos</h2>
-                <button onClick={(e) => this.evento(e, "asdfasdf")}>aa</button>{/*se hace asi solo cuando se vallan a pasar mas parametros*/}
-                <Boton myOnClick={(e) => this.evento(e, "asdfasdf")}/>{/*en caso de que sea para un componente ya hecho, se hace asi, como prop*/}
+                <button onClick={(e) => this.evento(e, "asdfasdf")}>aa</button>{/*Se hace asi solo cuando se vallan a pasar mas parametros*/}
+                <Boton myOnClick={(e) => this.evento(e, "asdfasdf")}/>{/*En caso de que sea para un componente ya hecho, se hace asi, como prop*/}
             </div>
         );
     }

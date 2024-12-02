99ffcd0cf258ce8b6baf997659536ff91697c8b0
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:26:32 +0100

    Update Formularios.jsx

--- src/componentsHooks/Formularios.jsx
+++ src/componentsHooks/Formularios.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-export function Formularios(props){//metodo para cuando no hayan muchos valores
+export function Formularios(props){//Metodo para cuando no hayan muchos valores
     const [nombre, setNombre] = useState("");
     const [cual, setCual] = useState("");
     const [elige, setElige] = useState("");
@@ -13,16 +13,16 @@ export function Formularios(props){//metodo para cuando no hayan muchos valores
     return(
         <>
             <h2>form</h2>
-            <form onSubmit={handleSubmit}>{/*funcion para cuando se haga el submit*/}
+            <form onSubmit={handleSubmit}>{/*Funcion para cuando se haga el submit*/}
                 <label htmlFor="nombre">nombre:</label>
-                <input type="text" id="nombre" name="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>{/*estableceria la variable de estado nombre en base a este input*/}
+                <input type="text" id="nombre" name="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>{/*Estableceria la variable de estado nombre en base a este input*/}
                 <p>radio</p>
-                <input type="radio" id="r1" name="cual" value="r1" onChange={(e) => setCual(e.target.value)}/><label htmlFor="">r1</label>{/*lo mismo con radios, valdria con checks*/}
+                <input type="radio" id="r1" name="cual" value="r1" onChange={(e) => setCual(e.target.value)}/><label htmlFor="">r1</label>{/*Lo mismo con radios, valdria con checks*/}
                 <input type="radio" id="r2" name="cual" value="r2" onChange={(e) => setCual(e.target.value)}/><label htmlFor="">r2</label>
                 <input type="radio" id="r3" name="cual" value="r3" onChange={(e) => setCual(e.target.value)}/><label htmlFor="">r3</label>
                 <input type="radio" id="r4" name="cual" value="r4" defaultChecked="true" onChange={(e) => setCual(e.target.value)}/><label htmlFor="">r4</label>
                 <p>elige</p>
-                <select name="elige" onChange={(e) => setElige(e.target.value)}>{/*lo mismo pero con select*/}
+                <select name="elige" onChange={(e) => setElige(e.target.value)}>{/*Lo mismo pero con select*/}
                     <option value="no">---</option>
                     <option value="op1">1</option>
                     <option value="op2">2</option>
@@ -33,15 +33,15 @@ export function Formularios(props){//metodo para cuando no hayan muchos valores
     );
 }
 
-export function FormulariosAvanzados(props){//metodo para cuando no hayan muchos valores
-    const [form, setForm] = useState({});//una sola variable de estado para todo el formulario
+export function FormulariosAvanzados(props){//Metodo para cuando no hayan muchos valores
+    const [form, setForm] = useState({});//Una sola variable de estado para todo el formulario
     
-    const handleChange = (e) => {//para todos los onChange
+    const handleChange = (e) => {//Para todos los onChange
         setForm({
             ...form, [e.target.name]: e.target.value
         });
     }
-    const handleChecked = (e) => {//para todos los checked/bool
+    const handleChecked = (e) => {//Para todos los checked/bool
         setForm({
             ...form, [e.target.name]: e.target.checked
         });

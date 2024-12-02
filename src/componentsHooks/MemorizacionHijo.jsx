3ce4ce5a19a99e495772e326bc6f71c36058b214
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:27:35 +0100

    Update MemorizacionHijo.jsx

--- src/componentsHooks/MemorizacionHijo.jsx
+++ src/componentsHooks/MemorizacionHijo.jsx
@@ -1,20 +1,20 @@
-import { memo, useMemo } from "react";//necesario menos useMemo
+import { memo, useMemo } from "react";//Necesario menos useMemo
 
-const ContadorHijo = (props) => {//representa una pieza de codigo que no deberia recargarse, como la respuesta de una peticion
+const ContadorHijo = (props) => {//Representa una pieza de codigo que no deberia recargarse, como la respuesta de una peticion
     console.log("renderiza por primera vez y ya");
 
     const pesado = useMemo(() => {
         let variablePesada;
         return variablePesada;
-    }, []);//pensado para lo mismo pero con variables pesadas
+    }, []);//Pensado para lo mismo pero con variables pesadas
 
     return(
         <>
             <p>menos</p>
             
-            {pesado}{/*de esta manera, la variable que devuelve pesado solo se renderizara la primera vez (por lo del useMemo)*/}
+            {pesado}{/*De esta manera, la variable que devuelve pesado solo se renderizara la primera vez (por lo del useMemo)*/}
         </>
     );
 }
 
-export default memo(ContadorHijo);//al exportarlo como memo, solo se renderizara la primera vez, sin recalcular
+export default memo(ContadorHijo);//Al exportarlo como memo, solo se renderizara la primera vez, sin recalcular

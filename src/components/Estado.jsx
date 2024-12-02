4c6e8bc1063fb50bcdd5ec48c16fed154bf9a62b
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:38:34 +0100

    Update Estado.jsx

--- src/components/Estado.jsx
+++ src/components/Estado.jsx
@@ -7,12 +7,12 @@ export default class Estado extends Component {
 
     constructor(props){
         super(props);
-        this.state = {//generar objeto state con sus propiedades
+        this.state = {//Generar objeto state con sus propiedades
             contador: 0
         }
 
         //setInterval(() => {
-            this.setState({//asi se cambian las propiedades del estado, entonces se vuelve a renderizar
+            this.setState({//Asi se cambian las propiedades del estado, entonces se vuelve a renderizar
                 contador: this.state.contador + 1
             });
         //}, 1000);
@@ -20,7 +20,7 @@ export default class Estado extends Component {
     render(){
         return(
             <div>
-                <h2>{this.state.contador}</h2>{/*ahora con el estado creado se accede a las propiedades asi*/}
+                <h2>{this.state.contador}</h2>{/*Ahora con el estado creado se accede a las propiedades asi*/}
             </div>
             
         );

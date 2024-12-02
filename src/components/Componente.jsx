9d06902f922fdce878c514539c2f743ea673447c
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:41:19 +0100

    Update Componente.jsx

--- src/components/Componente.jsx
+++ src/components/Componente.jsx
@@ -1,9 +1,9 @@
-//para crear componentes se hace en una carpeta aparte con archivos .js o .jsx
+//Para crear componentes se hace en una carpeta aparte con archivos .js o .jsx
 import React, {Component} from "react";
 
-class Componente extends Component{//crear el componente
-    render(){//dentro de este return se escriben las etiquetas
-        return <h2>titulo {this.props.propiedad}</h2>;//la propiedad deberia ser pasada a la hora de dibujar el componente
+class Componente extends Component{//Crear el componente
+    render(){//Dentro de este return se escriben las etiquetas
+        return <h2>titulo {this.props.propiedad}</h2>;//La propiedad deberia ser pasada a la hora de dibujar el componente
     }
 }
 

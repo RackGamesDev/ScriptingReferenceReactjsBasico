4e79074cfb5ae3934551737cde7ee7378e105e6b
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:23:17 +0100

    Update PageProductos.jsx

--- src/componentsRouter/pages/PageProductos.jsx
+++ src/componentsRouter/pages/PageProductos.jsx
@@ -3,16 +3,16 @@ import { useLocation } from "react-router-dom";
 
 const Productos = () => {
     let {search} = useLocation();
-    let query = new URLSearchParams(search);//query almacena todas las variables ?& de la url
-    let varA = query.get("variableA");//por cada posible variable
+    let query = new URLSearchParams(search);//Query almacena todas las variables ?& de la url
+    let varA = query.get("variableA");//Por cada posible variable
     let varB = query.get("variableB");
 
-    //let history = useHistory();//hace referencia al historial de esa pestagna  ya no va
+    //let history = useHistory();//Hace referencia al historial de esa pestagna  ya no va
     return(
         <>
             <h3>a= {varA}  b={varB}</h3>
         </>
     );
 }
-//http://localhost:5173/productos?variableA=123&variableB=3321  ejemplo
+//http://localhost:5173/productos?variableA=123&variableB=3321  Ejemplo
 export default Productos;

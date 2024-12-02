ffc7993d1cf17ec96cb7d3fe06a445eef03d836d
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:32:02 +0100

    Update ScrollHooks.jsx

--- src/componentsHooks/ScrollHooks.jsx
+++ src/componentsHooks/ScrollHooks.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';//tambien se usa useeffect
+import React, { useState, useEffect } from 'react';//Tambien se usa useeffect
 
 export default function ScrollHooks(props){
     const [scrollY, setScrollY] = useState(0);
 
     useEffect(()=>{
-        //aqui seria el codigo de cuando se monta el componente, tantas veces como se renderice
+        //Aqui seria el codigo de cuando se monta el componente, tantas veces como se renderice
 
         const detectarScroll = () => {
-            setScrollY(window.pageYOffset);//se declara una funcion al montar el componente que cambia el estado de scrollY
+            setScrollY(window.pageYOffset);//Se declara una funcion al montar el componente que cambia el estado de scrollY
         }
         window.addEventListener("scroll", detectarScroll);
         return()=>{
-            //esto se ejecuta cuando se desmonte el componente
+            //Esto se ejecuta cuando se desmonte el componente
             window.removeEventListener("scroll", detectarScroll);
         }
-    }, [scrollY]);//si se especifica este parametro, el useEffect solo se ejecutara cuando cambie esa variable (y al inicio)
+    }, [scrollY]);//Si se especifica este parametro, el useEffect solo se ejecutara cuando cambie esa variable (y al inicio)
 
     return(
         <>

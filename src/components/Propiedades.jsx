976a84b4dee99c2ae5d6deb21f5d0741d1bfbffa
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:35:42 +0100

    Update Propiedades.jsx

--- src/components/Propiedades.jsx
+++ src/components/Propiedades.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
-import PropTypes from "prop-types";//haciendo npm i -S prop-types
+import PropTypes from "prop-types";//Haciendo npm i -S prop-types
 
-export default function Propiedades(props){//otra manera sencilla de hacer el jsx
+export default function Propiedades(props){//Otra manera sencilla de hacer el jsx
     return(
         <div>
             <h1>propi {props.porDefecto}</h1>
@@ -10,11 +10,11 @@ export default function Propiedades(props){//otra manera sencilla de hacer el js
     );
 }
 
-Propiedades.defaultProps = {//se ponen propiedades por defecto, para el caso en el que no se especifiquen
+Propiedades.defaultProps = {//Se ponen propiedades por defecto, para el caso en el que no se especifiquen
     porDefecto: "aa"
 }
 
-//haciendo npm i -S prop-types
-Propiedades.propTypes = {//sirve para obligar a que las propiedades sean de un tipo especifico (PropTypes.tipo.(obligatoria?))
+//Haciendo npm i -S prop-types
+Propiedades.propTypes = {//Sirve para obligar a que las propiedades sean de un tipo especifico (PropTypes.tipo.(obligatoria?))
     otraVariable: PropTypes.number.isRequired
 }

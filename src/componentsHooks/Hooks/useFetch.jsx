55e8229d77399de2da51cc3832d4853358d36708
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 01:24:59 +0100

    Retocar

--- src/componentsHooks/Hooks/useFetch.jsx
+++ src/componentsHooks/Hooks/useFetch.jsx
@@ -1,12 +1,12 @@
-//cada hook personalizado va en un archivo distinto, normalmente en una carpeta aparte y tiene que empezar por "use"
+//Cada hook personalizado va en un archivo distinto, normalmente en una carpeta aparte y tiene que empezar por "use"
 import {useState, useEffect} from "react";
 
-export const useFetch = (url) => {//poniendo la funcion y todos los parametros que podria recibir
-    const [data, setData] = useState(null);//todos los datos que podria devolver
+export const useFetch = (url) => {//Poniendo la funcion y todos los parametros que podria recibir
+    const [data, setData] = useState(null);//Todos los datos que podria devolver
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {//procesamiento de datos
+    useEffect(() => {//Procesamiento de datos
         const getData = async (url) => {
             try{
                 let res = await fetch(url);
@@ -26,6 +26,6 @@ export const useFetch = (url) => {//poniendo la funcion y todos los parametros q
         getData(url);
     }, [url]);
 
-    return {data, isPending, error}//devolver los datos procesados
+    return {data, isPending, error}//Devolver los datos procesados
 }
-//en este caso es un hook para hacer peticiones get con una url, luego devuelve los datos
+//En este caso es un hook para hacer peticiones get con una url, luego devuelve los datos
